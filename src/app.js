28ec389c05e9c55c7c78d5196cf647c92ee3ffbc
lucas serpillo <88857502+lucasSerpillo@users.noreply.github.com>
2024-10-08 13:51:38 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuses = [
+    "Tengo que entregar un proyecto importante.",
+    "Mi perro se fue conduciendo una camioneta.",
+    "Un payaso me robo la mochila.",
+    "tengo programado un viaje a la luna.",
+
+    "Tengo que terminar de construir mi casa del arbol.",
+    "El perro se escapó.",
+
+    "debo regar mis plantas",
+    "Estaba ayudando a un amigo con un asunto de la mafia.",
+    "voy borracho",
+    "me despidieron del trabajo."
+  ];
+
+  document.getElementById("generate-button").addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * excuses.length);
+    document.getElementById("excusa").textContent = excuses[randomIndex];
+  });
 };
